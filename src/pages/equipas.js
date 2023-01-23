d61bfc2cbd596db771a3883cd98e3b85e91e8e3e
Juliana Gouveia <86081801+beelze-b0ss@users.noreply.github.com>
2023-01-23 15:17:06 +0000

    search

--- src/pages/equipas.js
+++ src/pages/equipas.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import { Box, Container, Grid, Pagination, Typography, Button } from "@mui/material";
+import { Box, Container, Grid, Card, CardContent, TextField, InputAdornment, SvgIcon } from "@mui/material";
+import { Search as SearchIcon } from "../icons/search";
+
 
 import { products } from "../__mocks__/products";
 import { ProductListToolbar } from "../components/equipas/equipas-toolbar";
@@ -12,62 +14,110 @@ import { useEffect, useState } from "react";
 
 const Page = () => {
   const [allDocs, setAllDocs] = useState([]);
+  const [search, setSearch] = useState("");
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    console.log(search)
+  };
 
   useEffect(() => {
     getInfo()
   }, [])
 
-  const getInfo = async () => { 
+  const getInfo = async () => {
     let temp = [];
-  
+
     const ref = await getDocs(collection(firestore, 'teams'));
     ref.forEach((doc) => {
-      
+
       // console.log(doc.id, " => ", doc.data())
       temp.push({
-        name : doc.data().name,
-        id : doc.data().id,
-        description : doc.data().description,
-        members : doc.data().users.length,
+        name: doc.data().name,
+        id: doc.data().id,
+        description: doc.data().description,
+        members: doc.data().users.length,
         users: [...doc.data().users]
       })
     })
-  
+
     console.log("DEBUG: ", temp);
     setAllDocs(temp);
   }
-  
-  return(
-  <>
-    <Head>
-      <title>Smart Break</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8,
-      }}
-    >
-       
-      <Container maxWidth={false}>
-        <ProductListToolbar />
-        <Box sx={{ pt: 3 }}>
-          {allDocs && allDocs.length == 0 ?
-          <></>
-          :
-            <Grid container spacing={3}>
-              {allDocs.map((product) => (
-                <Grid item key={product.id} lg={4} md={6} xs={12}>
-                  <ProductCard product={product}  />
-                </Grid>
-              ))}
-            </Grid>
-          }
-         
-        </Box>
-        {/* <Box
+
+  const filteredCustomers = allDocs.filter(team =>
+    team.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+
+  return (
+    <>
+      <Head>
+        <title>Smart Break</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+
+        <Container maxWidth={false}>
+          <ProductListToolbar />
+          <Box sx={{ mt: 3 }}>
+            <Card>
+              <CardContent>
+                <Box sx={{ maxWidth: 500 }}>
+                  <TextField
+                    fullWidth
+                    value={search}
+                    onChange={handleChange}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SvgIcon fontSize="small" color="action">
+                            <SearchIcon />
+                          </SvgIcon>
+                        </InputAdornment>
+                      ),
+                    }}
+                    placeholder="Procurar equipa"
+                    variant="outlined"
+                  />
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box sx={{ pt: 3 }}>
+            {allDocs && allDocs.length == 0 ?
+              <></>
+              :
+              <Grid container spacing={3}>
+                {search === "" ?
+                  <>
+                    {allDocs.map((product) => (
+                      <Grid item key={product.id} lg={4} md={6} xs={12}>
+                        <ProductCard product={product} />
+                      </Grid>
+                    ))}
+                  </>
+                  :
+                  <>
+                    {filteredCustomers.map((product) => (
+                      <Grid item key={product.id} lg={4} md={6} xs={12}>
+                        <ProductCard product={product} />
+                      </Grid>
+
+                    ))}
+                  </>}
+
+              </Grid>
+            }
+
+          </Box>
+          {/* <Box
           sx={{
             display: "flex",
             justifyContent: "center",
@@ -76,10 +126,11 @@ const Page = () => {
         >
           <Pagination color="primary" count={3} size="small" />
         </Box> */}
-      </Container>
-    </Box>
-  </>
-)};
+        </Container>
+      </Box>
+    </>
+  )
+};
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
