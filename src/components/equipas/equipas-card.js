5a6f813192f4f0a298255891973d5b65ef876c88
Joana Tavares <joanalt@ua.pt>
2023-01-23 17:53:27 +0000

    xx

--- src/components/equipas/equipas-card.js
+++ src/components/equipas/equipas-card.js
@@ -57,7 +57,7 @@ export const ProductCard = ({ product }) => {
               window.location.reload(false);
             }}
           >
-            Adicionar
+            Eliminar
           </Button>
         </DialogActions>
       </Dialog>
