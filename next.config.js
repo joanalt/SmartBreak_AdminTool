2ab09b56b50e1b0738997f6d11fd4a846e1baa67
Joana Tavares <joanalt@ua.pt>
2023-06-19 18:17:59 +0100

    teste 12

--- next.config.js
+++ next.config.js
@@ -4,11 +4,22 @@ module.exports = {
   eslint: {
     dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
-  async rewrites() {
+
+  async headers() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://api.example.com/:path*",
+        // matching all API routes
+        source: "https://sb-api.herokuapp.com",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
